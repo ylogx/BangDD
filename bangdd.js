1fb5f01a75802c6084504dfbde03d9a288c4e872
Shubham Chaudhary <shubham@chaudhary.xyz>
2019-05-17 15:31:42 -0700

    Insert in correct position

--- bangdd.js
+++ bangdd.js
@@ -1,38 +1,52 @@
-/*
-Just draw a border round the document.body.
-*/
-document.body.style.border = "5px solid red";
+const BANG_TO_ADD = "!g";
 
-let bang_it = document.createElement('a');
-bang_it.href = '#';
-bang_it.id = 'bang_it';
-bang_it.appendChild(document.createTextNode("Bang It"));
+function bangToAdd() {
+    return BANG_TO_ADD
+}
 
-bang_it.addEventListener("click", function(event) {
-    (function(event) {
-        let bang_to_add = " shubham chaudhary";
-        let search_form_input = document.getElementById("search_form_input");
-        let current_search = search_form_input.value;
-        search_form_input.value = current_search + bang_to_add;
-    }).call(bang_it, event);
-});
+function onTimeToBang(event) {
+    let search_form_input = document.getElementById("search_form_input");
+    let current_search = search_form_input.value;
+    let bang = bangToAdd();
 
-let search_form = document.getElementById("search_form");
-search_form.insertBefore(bang_it, search_form.getElementById("search_form_input"));
+    console.log("Current search contains bang" + current_search.contains(bang));
+    if (!current_search.contains(bang)) {
+        search_form_input.value = current_search + " " + bang;
+    }
+}
 
-// document.getElementById('search_form').insertBefore(bang_it, document.getElementById("search_form"));
+function createButton() {
+    let bang_it = document.createElement('a');
+    bang_it.href = '#';
+    bang_it.id = 'bang_it';
+    bang_it.appendChild(document.createTextNode("Bang It"));
 
-// document.getElementById('bang_it').addEventListener("click", function(event) {
-//     (function(event) {
-//         var bang_to_add = " shubham chaudhary";
-//         var current_search = document.getElementById("search_form_input").value;
-//         document.getElementById("search_form_input").value = current_search + bang_to_add;
-//     }).call(document.getElementById('bang_it'), event);
-// });
+    bang_it.addEventListener("click", function (event) {
+        (onTimeToBang).call(bang_it, event);
+    });
+    return bang_it;
+}
 
-// '<a href="#" id="bang_it">Google It</a>';
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
 
-// <a href="#" id="bang_it">Google It</a>
-// <script type="text/javascript">
-//
-// </script>
+function insertInCorrectPosition(bang_it) {
+    let existingButton = document.getElementById(bang_it.id);
+    if (existingButton) {
+        existingButton.remove();
+        console.log('Removed existing button')
+    }
+    // document.body.appendChild(bang_it);
+    // let search_form = document.getElementById("search_form");
+    // search_form.insertBefore(bang_it, document.getElementById("search_form_input"));
+    insertAfter(bang_it, document.getElementById("search_form"));
+    console.log('Inserted bang it element');
+}
+
+
+let bang_it = createButton();
+insertInCorrectPosition(bang_it);
+
+/* Just draw a border round the document.body. */
+document.body.style.border = "5px solid blue";
